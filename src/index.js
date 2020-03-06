a8291e497aa9be6afcee7d317e8f488b37a87881
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-03-06 18:44:25 +0200

    init: start Brackets task

---
+++ src/index.js
@@ -0,0 +1,26 @@
+module.exports = function check(str, bracketsConfig) {
+  let stack = [];
+  let first = bracketsConfig.map((item) => item[0]);
+  let second = bracketsConfig.map((item) => item[1]);
+  for(let i = 0; i < str.length; i++) {
+      let bracketIndex;
+    if ((bracketIndex = first.indexOf(str[i])) >= 0) {
+        if (first[bracketIndex] === second[bracketIndex]) {
+            if (stack[stack.length - 1] === first[bracketIndex]) {
+                stack.pop();
+            } else {
+                stack.push(str[i]);
+            }
+        } else {
+           stack.push(str[i]);  
+        }
+    } else if (( bracketIndex = second.indexOf(str[i])) >= 0) {
+        if (stack.length > 0 && stack[stack.length - 1] === first[bracketIndex]) {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+  }
+  return !stack.length;
+}
